fdb79b40d8d561cef876259c8763a5e8bf57504c
cloudmsg <karl@cloudmsg.com>
2014-06-18 16:15:12 -0400

    added are you sure to restoredb

--- restoreDB.bat
+++ restoreDB.bat
@@ -1,1 +1,8 @@
+@echo off
+setlocal
+:PROMPT
+SET /P AREYOUSURE=Are you sureou want to restore the database? It will overwrite the current database! (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 "C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql.exe" -ucloudmsg -p2T^^p%%AE! -hlocalhost panachebeadco_new < db.sql
+:END
+endlocal y
