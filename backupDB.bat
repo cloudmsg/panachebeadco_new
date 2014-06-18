0e2185312794032e773870d978aabcaedefdd84a
cloudmsg <karl@cloudmsg.com>
2014-06-18 14:15:08 -0400

    created bat files for backing up and restoring db

---
+++ backupDB.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\MySQL\MySQL Server 5.6\bin\mysqldump.exe" -ucloudmsg -p2T^^p%%AE! -hlocalhost --skip-extended-insert panachebeadco_new > db.sql
---
+++ restoreDB.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql.exe" -ucloudmsg -p2T^^p%%AE! -hlocalhost panachebeadco_new < db.sql
